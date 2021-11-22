0e47b5f9d2ea39720b52d8881fe862602a792b41
mellokx <hernanvicentecaro@gmail.com>
2021-11-21 21:14:27 -0300

    parte 1 terminada

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ void * hebra(void * arg){
     for(int i = 0; i<p->M; ++i){
         r = rand()%11 + 1;
         sleep(r);
-        printf("Tarea %d terminada por hebra %d en %d segundos\n",p->tareas_por_etapa[i],p->num_hebra,r);
+        printf("    Tarea #%d terminada por hebra %d en %d segundos\n",p->tareas_por_etapa[i],p->num_hebra,r);
         pthread_barrier_wait (&barrera);
     }
 }
@@ -43,6 +43,7 @@ void sincronizacion_con_barrier(int N,int M){
         printf("%d\n",vec_hebras[i]);
     }
     for(int i = 0; i<M; ++i){
+        printf("--------------------\n",i+1);
         printf("Etapa %d iniciada...\n",i+1);
         pthread_barrier_wait (&barrera);
         printf("Etapa %d terminada.\n",i+1);
